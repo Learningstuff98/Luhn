4f5d975f378439158c39b4955a2d843a2a4b7531
Andy Strube <andystrube64@gmail.com>
2019-06-04 20:55:38 -0700

    Another refactor

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,43 +1,29 @@
+class Array
+  def double_digits
+    map.with_index { |num, index| index.odd? ? num * 2 : num }
+  end
+
+  def remove_nine
+    map { |num| num >= 10 ? num - 9 : num }
+  end
+
+  def reverse_method
+    reverse
+  end
+
+  def sum_digits
+    sum
+  end
+end
+
 module Luhn
   def self.is_valid?(card_number)
-    card_number = split_method(card_number)
-    card_number = reverse_method(card_number)
-    card_number = double_digits(card_number)
-    card_number = remove_nine(card_number)
-    card_number = sum_digits(card_number)
+    card_number = split_method(card_number).reverse_method.double_digits.remove_nine.sum_digits
     card_number % 10 == 0
   end
 
   def self.split_method(card_number)
     card_number = card_number.to_s
     card_number = card_number.split('').collect{|i| i.to_i}
-  end
-
-  def self.reverse_method(num_array)
-    num_array = num_array.reverse
-  end
-
-  def self.double_digits(num_array)
-    num_array.map.with_index do |num, index|
-      if index.odd?
-        num *= 2
-      else
-        num
-      end
-    end
-  end
-
-  def self.remove_nine(num_array)
-    num_array.map do |num|
-      if num >= 10
-        num -= 9
-      else
-        num
-      end
-    end
-  end
-
-  def self.sum_digits(num_array)
-    num_array.sum
   end
 end
