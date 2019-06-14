c598c5d9081d05d11055613e1d8d86b5c199eb82
Andy Strube <andystrube64@gmail.com>
2019-06-13 17:24:17 -0700

    Some refactoring

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,14 +1,14 @@
 require_relative 'class_method_addon'
 
 module Luhn
-  def self.is_valid?(card_number)
+  def self.valid?(card_number)
     card_number = split_method(card_number).reverse_method.double_digits
     card_number = card_number.remove_nine.sum_digits
-    card_number % 10 == 0
+    (card_number % 10).zero?
   end
 
   def self.split_method(card_number)
     card_number = card_number.to_s
-    card_number = card_number.split('').collect{|i| i.to_i}
+    card_number.split('').collect { |i| i.to_i }
   end
 end
--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -1,40 +1,40 @@
 require 'spec_helper'
 
 RSpec.describe Luhn, type: :model do
-  describe 'is_valid? should work' do
+  describe 'valid? should work' do
     it 'should return true if the number is valid' do
-      expect(Luhn.is_valid?(4194560385008504)).to be true
+      expect(Luhn.valid?(4194560385008504)).to be true
     end
 
     it 'should return false if the number is not valid' do
-      expect(Luhn.is_valid?(4194560385008505)).to be false
+      expect(Luhn.valid?(4194560385008505)).to be false
     end
 
     # some credit card numbers are 15 digits
     # these tests confirm the code is starting on the right side, not the left
     it 'should return true if the number is valid and the number has an odd number of digits' do
-      expect(Luhn.is_valid?(377681478627336)).to be true
+      expect(Luhn.valid?(377681478627336)).to be true
     end
 
     it 'should return false if the number is not valid and the number has an odd number of digits' do
-      expect(Luhn.is_valid?(377681478627337)).to be false
+      expect(Luhn.valid?(377681478627337)).to be false
     end
 
     # These tests are from codewars
     it 'should be false for this number from codewars' do 
-      expect(Luhn.is_valid?(1234567890123456)).to be false
+      expect(Luhn.valid?(1234567890123456)).to be false
     end
 
     it 'should be true for this number from codewars' do 
-      expect(Luhn.is_valid?(4408041234567893)).to be true
+      expect(Luhn.valid?(4408041234567893)).to be true
     end
 
     it 'should be true for this number from codewars' do 
-      expect(Luhn.is_valid?(38520000023237)).to be true
+      expect(Luhn.valid?(38520000023237)).to be true
     end
 
     it 'should be true for this number from codewars' do 
-      expect(Luhn.is_valid?(4222222222222)).to be true
+      expect(Luhn.valid?(4222222222222)).to be true
     end
   end
 end
